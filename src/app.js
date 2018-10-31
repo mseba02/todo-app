3f5e2d37fea20f7d980a721cdc0907d07a0c8ed2
Seba Marius <marius.seba@gmail.com>
2018-10-31 14:11:27 +0200

    Update src/app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
+import Layout from './components/layout';
 import AddTask from './components/addTask';
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <Layout>
           <AddTask/>
-      </div>
+      </Layout>
     );
   }
 }
