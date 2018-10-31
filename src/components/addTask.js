70f814613b48415920c01b31017ca68cfc7d322d
Seba Marius <marius.seba@gmail.com>
2018-10-31 14:11:24 +0200

    Update src/components/addTask.js

--- src/components/addTask.js
+++ src/components/addTask.js
@@ -8,6 +8,9 @@ class addTask extends Component{
 
         this.state = {
             active: false,
+            todotitle: '',
+            tododate: '',
+            todopriority: ''
         };
     }
     // button trigger
@@ -16,6 +19,28 @@ class addTask extends Component{
         this.setState({ active: !currentState });
         console.log(this.state.active);
     };
+    // add to do
+    handleAddTodo= (e) => {
+      e.preventDefault();
+      const titletodo = this.state.todotitle;
+      console.log(titletodo);
+
+    };
+    // to do title
+    handleTDTitle = (e) => {
+        const value = e.target.value;
+        this.setState({
+            todotitle: value
+        });
+        console.log(e.target.value)
+
+    };
+    handleTDate = (e) => {
+        console.log(e.target.value)
+    };
+    handleTDPriority = (e) => {
+        console.log(e.target.value)
+    };
     render(){
         let className = classNames({
             addTask: true,
@@ -23,14 +48,35 @@ class addTask extends Component{
         });
         return (
             <div>
-                <div className={className}>
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
-                    <input type="text" />
+                <div className="today-todolist">
+                    <h1>Today's to-do-list</h1>
+                    <div>to do 1 ...</div>
                 </div>
+                <form className={className} onSubmit={this.handleAddTodo}>
+                    <input
+                        className="to-do-name"
+                        type="text"
+                        placeholder="Enter To-do"
+                        onChange={this.handleTDTitle}
+                    />
+                    <input
+                        type="text"
+                        placeholder="Pick Date"
+                        onChange={this.handleTDate}
+                    />
+                    <div className="form-select">
+                        <label>Priority</label>
+                        <select onChange={this.handleTDPriority}>
+                            <option>Low</option>
+                            <option>Medium</option>
+                            <option>High</option>
+                            <option>Critical</option>
+                        </select>
+                    </div>
+                    <button type="submit">Add To do</button>
+                </form>
                 <div className="buttonToggle">
-                    <button  onClick={this.toggleClass}>+</button>
+                    <button onClick={this.toggleClass}>+</button>
                 </div>
             </div>
         )
