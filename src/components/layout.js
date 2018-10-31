4e954ebce35d6051501288ca8d95e1c1f1420181
Seba Marius <marius.seba@gmail.com>
2018-10-31 14:11:20 +0200

    Update src/components/layout.js

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+// import
+import React from 'react';
+
+// layout
+const Layout = ( (props) =>(
+        <div className="main-layout">{props.children}</div>
+    )
+);
+
+// export
+export default Layout;
